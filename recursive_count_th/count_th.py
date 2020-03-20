0381d3631d4d223d976e7617547cf98cf720fdf8
Anthony Hill <hillan1152@gmail.com>
2020-03-20 14:00:58 -0400

    figured out count_th, but didn't finish robot

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,27 +7,40 @@ Your function must utilize recursion. It cannot contain any loops.
 #   Passes a word in the function that COUNTS number of times "th" pops up
 #           case will matter
 #   only use recursion NO LOOPS
+import math
 
 
 def count_th(word):
     # Plan
-    #   create counter
-    count = 0
-    #   create base cases: word needs to be at least 2 letters aka "th"
-    if len(word) < 2:
-        return count == 0
-    else:
-        #   needs to look at word in 2's
-        if word[:2] == "th":
-            count += 1
-        #   if "th":
-        #       count += 1
-        #   return count
-        # TBC
-        else:
-            count_th(1 + word[:2])
+    #   create counter thought for loop, couldn't do it, so just added a digit.
 
-    return count
+    #   create base case: word needs to be at least 2 letters aka "th"
+    if len(word) >= 2:
+        #  needs to look at letters in 2's for lowercase "th"
+        if word[:2] == "th":
+            # if its true return 1 and restart recursion at the next available letter
+            return 1 + count_th(word[1:])
+        else:
+            # if the next two letters != "th", restart recursion at the next available letter
+            return count_th(word[1:])
+    else:
+        # if the length of the remaining letters goes below 2, return 0 and end recursion.
+        return 0
+
+
+# def other_count(word, count):
+#     if len(word) >= 2:
+#         #   needs to look at word in 2's
+#         if word[:2] == "th":
+#             count += 1
+#             return other_count(word[1:], count)
+#         else:
+#             return other_count(word[1:], count)
+#     else:
+#         return 0
+
+# other_count(word, count)
+# return count
 
 
 print(count_th("thwethab"))
