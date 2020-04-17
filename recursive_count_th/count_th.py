fe8d3ce0b9b34e5c2c020e611b38dbb52a06ca5a
Anthony Hill <hillan1152@gmail.com>
2020-04-17 11:40:02 -0400

    completed short answer and count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,46 +1,33 @@
+
 '''
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-# UNDERSTAND:
-#   Passes a word in the function that COUNTS number of times "th" pops up
-#           case will matter
-#   only use recursion NO LOOPS
-import math
+# UNDERSTAND
+#       Must find instances of lowercase 'th'
+#       Go through word 2 letters at a time
+#       Case matters
+#       only recursion
 
 
 def count_th(word):
-    # Plan
-    #   create counter thought for loop, couldn't do it, so just added a digit.
-
-    #   create base case: word needs to be at least 2 letters aka "th"
-    if len(word) >= 2:
-        #  needs to look at letters in 2's for lowercase "th"
-        if word[:2] == "th":
-            # if its true return 1 and restart recursion at the next available letter
-            return 1 + count_th(word[1:])
-        else:
-            # if the next two letters != "th", restart recursion at the next available letter
-            return count_th(word[1:])
-    else:
-        # if the length of the remaining letters goes below 2, return 0 and end recursion.
-        return 0
-
-
-# def other_count(word, count):
-#     if len(word) >= 2:
-#         #   needs to look at word in 2's
-#         if word[:2] == "th":
-#             count += 1
-#             return other_count(word[1:], count)
-#         else:
-#             return other_count(word[1:], count)
-#     else:
-#         return 0
-
-# other_count(word, count)
-# return count
+    # keep count of th
+    count = 0
+    # Base Case, length can no go below 2
+    if len(word) < 2:
+        return count
+    # Does the word even exist?
+    if len(word):
+        two_letters = word[0:2]
+        #   Do the next two letters contain th
+        if two_letters == 'th':
+            # add one
+            count += 1
+        # recall count_th and add count
+        return count_th(word[1:]) + count
 
 
-print(count_th("thwethab"))
+a = [1, 2, 3, 4]
+print("Move to next number", a[1:])
+print("2 digits? --> ", a[0:2])
